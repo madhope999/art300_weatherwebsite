48640d8b25d447856856ee4afcdf643fe58014ee
madhope999 <60238943+madhope999@users.noreply.github.com>
2020-04-20 18:45:07 -0500

    added icons

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
 var Keys = {
-    openweathermap: "3e8ee2b042981324e7d39449abd84160"
+    openweathermap: "3e8ee2b042981324e7d39449abd84160",
     weatherbit: "87747e00c0e7414d8e129d4bdd6499cb"
-    
-}
+} ;
